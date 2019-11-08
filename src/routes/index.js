8d63f50d7e14ac9c831d6ccb16d7253ed6ff9195
Nicolas Sentout <nicolas.sentout@hotmail.com>
2019-11-08 12:15:41 +0100

    release v0.1.0

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ModelProject = require('../models/project');
+const router = express.Router();
+//Must add to every resource access page
+const { ensureAuthenticated } = require('../config/authenticated');
+
+router.get('/', ensureAuthenticated,(req, res) => {
+    res.redirect('/Projects');
+});
+//Passing ensureAthenticated
+router.get('/Projects',ensureAuthenticated, (req, res) => {
+    var projects;
+    ModelProject.find({ 'users.email' : req.user.email })
+    .then(projects => {
+        res.render('index', {
+            user: req.user,
+            projects : projects
+        });
+    }).catch(err => console.log(err));
+    
+    
+});
+module.exports = router;
